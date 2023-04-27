aafd78aba8e86613abad95e91760b7a74d3edf9a
JosieSavill <fiordasavilljosie@gmail.com>
2023-04-27 03:56:26 -0700

    created js/login.js

---
+++ public/js/login.js
@@ -0,0 +1,32 @@
+const { doc } = require("prettier");
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const usernameInput = document.querySelector('#username-input');
+    const passwordInput = document.querySelector('#password-input');
+
+
+    // send a POST request to the server with the user's credentials
+    const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: usernameInput.value,
+            password: passwordInput.value 
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        // if login is usccessfu, redirect to the dashboard page
+        document.location.replace('/dashboard');
+    } else {
+        // if login fails, display error
+        const errorMessage = await response.text();
+        const errorAlert = document.querySelector('#error-alert');
+        errorAlert.textContent = errorMessage;
+        errorAlert.classList.remove('d-none');
+    }
+};
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
